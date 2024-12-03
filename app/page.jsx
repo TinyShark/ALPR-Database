93ce8728fb34f9f53adaa30bae7a6dabe36b4f17
TinyShark <seb@tinyshark.co.uk>
2024-12-03 18:46:46 +0000

    basepath fixes

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,9 @@ import PlateTable from "@/components/PlateTable";
 import { ThemeToggle } from "@/components/ThemeToggle";
 import DashboardLayout from "@/components/layout/MainLayout";
 import { redirect } from "next/navigation";
-import { withBasePath } from "@/lib/utils";
+import { getBasePath } from '@/lib/serverUtils';
 
 export default async function Home() {
-  redirect(withBasePath("/dashboard"));
+  const basePath = await getBasePath();
+  redirect(`${basePath}/dashboard`);
 }
