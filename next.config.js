71fd9147779812a4950561f25f84bde376d9b0d1
TinyShark <seb@tinyshark.co.uk>
2024-12-03 19:29:39 +0000

    ..

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
+const { getConfig } = require('./lib/settings');
+
 const nextConfig = {
+  // Get the base path from settings
+  async rewrites() {
+    const config = await getConfig();
+    const basePath = config.homeassistant?.basePath || '';
+    
+    return {
+      beforeFiles: [
+        {
+          source: '/_next/:path*',
+          destination: `${basePath}/_next/:path*`,
+        },
+      ],
+    };
+  },
   outputFileTracingIncludes: {
     "/**": [
       "/.next",
